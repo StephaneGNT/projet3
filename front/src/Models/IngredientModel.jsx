7775fc194760a3448ad65305d7cf4966fa3acc16
Stéphane Guinot <sguinot86@gmail.com>
2018-11-19 15:54:05 +0100

    model

--- front/src/Models/IngredientModel.jsx
+++ front/src/Models/IngredientModel.jsx
@@ -1,12 +1,17 @@
 class IngredientModel {
-  constructor(name, category, price, availability, image, infobulle) {
+  constructor(name, category, price, availability, image, infobulle, allergies) {
     this.name = name;
     this.category = category;
     this.price = price;
     this.availability = availability;
     this.image = image;
     this.infobulle = infobulle;
+    this.allergies = allergies
   }
 }
+
+let carrotCake = new IngredientModel('carrot cake','base',2,true,url_image_carrot_cake,"un délicieux gâteau aux carottes", "Carottes");
+let ingredients = [];
+ingredients.push(carrotCake);
 
 export default IngredientModel;
