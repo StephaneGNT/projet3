e4075c18362113cb3593d35bd8538253b235a89a
Mathieu De Armey <mdearmey@gmail.com>
2018-11-21 17:20:22 +0100

    dev modifiée à tort

--- front/src/Containers/Public/Composition.jsx
+++ front/src/Containers/Public/Composition.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Ingredient from './Ingredient';
-
+import Price from './Price';
 import '../../Assets/Styles/Composition.css';
 
-const mapStateToProps = state => {
-  console.log(state)
-  return(
-  {
-    elementToDisplay: state.cakeBases,
-    cake: state.cakeCharacteristics, 
-  }
-);}
+const mapStateToProps = state => ({
+  elementToDisplay: state.cakeBases,
+  cake: state.cakeCharacteristics,
+  price: state.price,
+});
 
-const mapDispatchToProps = () => ({});
+// const mapDispatchToProps = () => ({});
 
 const renderElement = (data) => {
   const render = [];
@@ -23,22 +20,27 @@ const renderElement = (data) => {
     (ingredient) => {
       if (ingredient.dispo /*&& compatibleIngredients.indexOf(ingredient.name) >= 0*/) render.push(<Ingredient ingredient={ingredient} />);
     },
-  );
+    );
   return render;
   // return data.map(ingredient => <Ingredient ingredient={ingredient} />);
 };
 
-const Composition = ({ elementToDisplay }) => (
-  <div className="row" style={{ width: '1OO%', height: '100%' }}>
-    <div className="col-8 ingredientDisplay">
-      {renderElement(elementToDisplay)}
+const Composition = ({ elementToDisplay, price }) => (
+  <div>
+    <div>
+      <Price amount={price} />
     </div>
-    <div className="col-4 cakeDisplay">
+    <div className="row" style={{ width: '1OO%', height: '100%' }}>
+      <div className="col-8 ingredientDisplay">
+        {renderElement(elementToDisplay)}
+      </div>
+      <div className="col-4 cakeDisplay">
+      </div>
     </div>
   </div>
 );
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  // mapDispatchToProps,
 )(Composition);
