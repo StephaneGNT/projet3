8e87a8dfa4870800ffe479c18404b91461ebb752
Emeric <emeric555@gmail.com>
2018-11-21 11:54:15 +0100

    commit

---
+++ front/src/Containers/Public/CakeInProgress.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class CakeInProgress extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div style={{border:'1px solid black'}}> </div>
+    );
+  }
+}
+
