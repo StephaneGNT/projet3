2fdd7bd020885c801f81fae2977d40cf29d316fd
Stéphane Guinot <sguinot86@gmail.com>
2018-11-28 08:49:09 +0100

    routes corrigées

--- front/src/Containers/Public/Composition.jsx
+++ front/src/Containers/Public/Composition.jsx
@@ -10,10 +10,6 @@ const allowDrop = (ev) => {
   ev.preventDefault();
 };
 
-const drag = (ev) => {
-  ev.dataTransfer.setData('text', ev.target.id);
-};
-
 const drop = (ev) => {
   ev.preventDefault();
   const data = ev.dataTransfer.getData('text');
--- front/src/Containers/Public/Ingredient.jsx
+++ front/src/Containers/Public/Ingredient.jsx
@@ -4,12 +4,26 @@ import { Col } from 'reactstrap';
 import '../../Assets/Styles/Ingredient.css';
 
 const Ingredient = (props) => {
-  const description = `${props.ingredient.info} 
+  const getDescription = () => {
+    let description;
+    if (props.ingredient.portion) {
+      description = `${props.ingredient.info} 
+Allergènes: ${props.ingredient.allerg}
+Giluna recommande une portion de ${props.ingredient.portion}`;
+    } else {
+      description = `${props.ingredient.info} 
 Allergènes: ${props.ingredient.allerg}`;
+    }
+    return description;
+  };
+
+  const drag = (ev) => {
+    ev.dataTransfer.setData('text', ev.target.id);
+  };
 
   return (
     <Col className="ingredient">
-      <img src={props.ingredient.img} title={description} draggable="true" alt="" />
+      <img src={props.ingredient.img} title={getDescription()} draggable="true" onDragStart={ev => drag(ev)} alt="" />
       <p>{props.ingredient.name}</p>
     </Col>
   );
