06c4cf6807fa93230840d8448d956d42e1dcff64
Philipp Elsässer <philipp-elsaesser@outlook.com>
2018-12-06 10:10:08 +0100

    added date of bith input to Userinfo.jsx

--- front/src/Containers/Public/UserInfo.jsx
+++ front/src/Containers/Public/UserInfo.jsx
@@ -14,6 +14,7 @@ class UserInfo extends Component {
       lastname: '',
       email: '',
       telephone: '',
+      birthdate: '',
     };
   }
 
@@ -31,6 +32,9 @@ class UserInfo extends Component {
       case 'telephone':
         this.setState({ telephone: e.target.value });
         break;
+      case 'birthdate':
+        this.setState({ birthdate: e.target.value });
+        break;
       default:
         return null;
     }
@@ -44,7 +48,10 @@ class UserInfo extends Component {
   }
 
   activateButton = () => {
-    if (this.state.firstname.length > 1 && this.state.lastname.length > 1 && this.state.telephone.length >= 10 && this.validEmail(this.state.email) === false) return false;
+    if (this.state.firstname.length > 1
+        && this.state.lastname.length > 1
+        && this.state.telephone.length >= 10
+        && this.validEmail(this.state.email) === false) return false;
     return true;
   }
 
@@ -55,17 +62,38 @@ class UserInfo extends Component {
           <Progressbar />
         </Row>
         <Row className="justify-content-around">
-          <Col sm="10" md="5">
+          <Col sm="10" md="4">
             <FormGroup>
               <Label for="firstname">
-                <span className="text-danger lead">* </span>
+                <span className="text-danger">* </span>
                 Prénom
               </Label>
               <Input type="text" name="firstname" id="firstname" placeholder="votre prénom" value={this.state.firstname} onChange={e => this.updateState(e)} />
             </FormGroup>
+          </Col>
+          <Col sm="10" md="4">
+            <FormGroup>
+              <Label for="lastname">
+                <span className="text-danger">* </span>
+                Nom
+              </Label>
+              <Input type="text" name="lastname" id="lastname" placeholder="votre nom de famille" value={this.state.lastname} onChange={e => this.updateState(e)} />
+            </FormGroup>
+          </Col>
+          <Col sm="10" md="3">
+            <FormGroup>
+              <Label for="birthdate">
+                Date de naissance
+              </Label>
+              <Input type="text" name="birthdate" id="birthdate" placeholder="votre date de naissance" value={this.state.birthdate} onChange={e => this.updateState(e)} />
+            </FormGroup>
+          </Col>
+        </Row>
+        <Row className="justify-content-around">
+          <Col sm="10" md="6">
             <FormGroup>
               <Label for="email">
-                <span className="text-danger lead">* </span>
+                <span className="text-danger">* </span>
                 E-mail
               </Label>
               <Input invalid={this.validEmail(this.state.email)} type="email" name="email" id="email" placeholder="votre adresse mail" value={this.state.email} onChange={e => this.updateState(e)} />
@@ -74,15 +102,8 @@ class UserInfo extends Component {
           </Col>
           <Col sm="10" md="5">
             <FormGroup>
-              <Label for="lastname">
-                <span className="text-danger lead">* </span>
-                Nom
-              </Label>
-              <Input type="text" name="lastname" id="lastname" placeholder="votre nom de famille" value={this.state.lastname} onChange={e => this.updateState(e)} />
-            </FormGroup>
-            <FormGroup>
               <Label for="telephone">
-                <span className="text-danger lead">* </span>
+                <span className="text-danger">* </span>
                 Téléphone
               </Label>
               <Input type="text" name="telephone" id="telephone" placeholder="votre numéro de téléphone" value={this.state.telephone} onChange={e => this.updateState(e)} />
@@ -91,7 +112,7 @@ class UserInfo extends Component {
         </Row>
         <Row className="text-center">
           <NavArrowsLayout />
-          <Button className="btn btn-info btn-lg" disabled={this.activateButton()}>envoyer la Commande</Button>
+          <Button className="btn btn-info btn-lg ml-3" disabled={this.activateButton()}>envoyer la Commande</Button>
         </Row>
       </Container>
     );
