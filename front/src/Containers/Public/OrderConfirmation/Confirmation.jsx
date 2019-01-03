83163822f2057e14cade52d96848a0b11d62e245
Stéphane Guinot <sguinot86@gmail.com>
2019-01-03 09:59:07 +0100

    provisoire

--- front/src/Containers/Public/OrderConfirmation/Confirmation.jsx
+++ front/src/Containers/Public/OrderConfirmation/Confirmation.jsx
@@ -32,17 +32,7 @@ class Confirmation extends Component {
     // else if (decoration || customMessage) {
     //   if (decoration) description += `Décoration : ${decoration}`;
     //   else description += `Décoration : ${customMessage}`;
-    // let message = '';
 
-    // if (occasion) message += `Occasion: ${occasion}`;
-    // message += `Commande: ${quantity} ${type}`;
-    // if (typeof (size) === 'number') message += ` pour ${size} personnes`;
-    // else message += ` de taille ${size}`;
-    // if (decoration || customMessage) message += `Personnalisation : ${decoration} ${customMessage}`;
-    // message += `Composition : ${(ingredients.map(item => item.name))}`;
-    // if (deliveryDate) message += `Date de retrait : ${moment(deliveryDate).format('Do MMMM YYYY')}`;
-
-    // return message;
 
     if (type === 'brownie' || type === 'cookie') {
       return (
@@ -160,20 +150,6 @@ class Confirmation extends Component {
         </p>
       );
     }
-
-    // if (type === 'macarons') {
-    //   const flavor = ingredients.map((ingredient) => {
-    //     if (ingredient.type === 'Parfum') return ingredient.name;
-    //   });
-    //   const color = ingredients.map((ingredient) => {
-    //     if (ingredient.type === 'Coquille') return ingredient.name;
-    //   });
-    //   description += `Parfum : ${flavor} - Couleur : ${color}`;
-    // } else description += `${ingredients.map(item => item.name)} `;
-
-    // if (deliveryDate) description += `Date de retrait : ${moment(deliveryDate).format('Do MMMM YYYY')}`;
-
-    // return description;
   }
 
   render() {
