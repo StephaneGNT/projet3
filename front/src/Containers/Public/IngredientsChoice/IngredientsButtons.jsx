e196c9c88ae5c0687773167dd057aa4704fcc2cd
Stéphane Guinot <sguinot86@gmail.com>
2019-01-03 10:19:01 +0100

    command button

--- front/src/Containers/Public/IngredientsChoice/IngredientsButtons.jsx
+++ front/src/Containers/Public/IngredientsChoice/IngredientsButtons.jsx
@@ -2,23 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Row } from 'reactstrap';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import NavArrowsLayout from '../Navigation/NavArrowsLayout';
 import { updateIndex } from '../../../Actions/cakeActions/changeIndex';
 
 import '../../../Assets/Styles/IngredientsButtons.css';
 
 class IngredientsButtons extends Component {
-  renderButton = (index, cake) => {
-    let render;
-    return render;
-  };
 
-  checkEnable = (event, disabled) => {
-    const { cake, setPageIndex } = this.props;
-    const newIndex = (cake.type === 'cake' || cake.type === 'cheesecake') ? 5 : 3;
-    if (disabled) event.preventDefault();
-    else setPageIndex(newIndex);
+  redirect = () => {
+    console.log("bouton cliqué")
+    return <Redirect to="/admin/customCake" />
   };
 
   render() {
@@ -26,12 +20,9 @@ class IngredientsButtons extends Component {
     const disabled = !cake.ingredients.length > 0;
     return (
       <Row className="back-btn">
-        <button type="button" className="order-btn" disabled={disabled}>
-          <Link className="commandLink" onClick={e => this.checkEnable(e, disabled)} to="/mycake/customCake">
-            Commander
-          </Link>
+        <button type="button" className="order-btn" onClick={() => this.redirect()}>
+          Test
         </button>
-        {this.renderButton(index, cake)}
         <NavArrowsLayout />
       </Row>
     );
@@ -41,7 +32,6 @@ class IngredientsButtons extends Component {
 IngredientsButtons.propTypes = {
   index: PropTypes.number.isRequired,
   cake: PropTypes.shape({}).isRequired,
-  setPageIndex: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
--- front/src/Containers/Public/OrderConfirmation/Confirmation.jsx
+++ front/src/Containers/Public/OrderConfirmation/Confirmation.jsx
@@ -83,7 +83,7 @@ class Confirmation extends Component {
         </p>
       );
     }
-    if (type === 'cheesecake') {
+    if (type === 'cheesecake' || type === 'cake') {
       return (
         <p>
         Occasion :
@@ -106,7 +106,7 @@ class Confirmation extends Component {
           {' '}
           <br />
           <br />
-        La composition de votre cheesecake:
+        Composition:
           {(ingredients.map(item => <div>{item.name}</div>))}
           <br />
           <br />
@@ -160,20 +160,6 @@ class Confirmation extends Component {
         </p>
       );
     }
-
-    // if (type === 'macarons') {
-    //   const flavor = ingredients.map((ingredient) => {
-    //     if (ingredient.type === 'Parfum') return ingredient.name;
-    //   });
-    //   const color = ingredients.map((ingredient) => {
-    //     if (ingredient.type === 'Coquille') return ingredient.name;
-    //   });
-    //   description += `Parfum : ${flavor} - Couleur : ${color}`;
-    // } else description += `${ingredients.map(item => item.name)} `;
-
-    // if (deliveryDate) description += `Date de retrait : ${moment(deliveryDate).format('Do MMMM YYYY')}`;
-
-    // return description;
   }
 
   render() {
