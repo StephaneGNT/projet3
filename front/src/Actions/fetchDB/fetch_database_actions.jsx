161065649c0baa46ce8d17bfa7c355ef3eb88f90
Philipp Elsässer <philipp-elsaesser@outlook.com>
2019-01-08 12:15:27 +0100

    created Actions to fetch incredients from database

---
+++ front/src/Actions/fetchDB/fetch_database_actions.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+export const getCakeBases = () => (dispatch => (
+  axios.get('/api/cakebases')
+    .then(data => dispatch({ type: 'GET_CAKE_BASES', payload: data }))
+));
+
+export const getFillings = () => (dispatch => (
+  axios.get('/api/fillings')
+    .then(data => dispatch({ type: 'GET_FILLINGS', payload: data }))
+));
+
+export const getIcings = () => (dispatch => (
+  axios.get('/api/icings')
+    .then(data => dispatch({ type: 'GET_ICINGS', payload: data }))
+));
+
+export const getToppings = () => (dispatch => (
+  axios.get('/api/toppings')
+    .then(data => dispatch({ type: 'GET_TOPPINGS', payload: data }))
+));
+
+export const getCheesecakeFlavor = () => (dispatch => (
+  axios.get('/api/cheesecakeFlavor')
+    .then(data => dispatch({ type: 'GET_CHEESECAKE_FLAVOR', payload: data }))
+));
+
+export const getCookieBases = () => (dispatch => (
+  axios.get('/api/cookieBases')
+    .then(data => dispatch({ type: 'GET_COOKIE_BASES', payload: data }))
+));
+
+export const getMacaronShells = () => (dispatch => (
+  axios.get('/api/macaronShells')
+    .then(data => dispatch({ type: 'GET_MACARON_SHELLS', payload: data }))
+));
+
+export const getMacaronFlavors = () => (dispatch => (
+  axios.get('/api/macaronFlavors')
+    .then(data => dispatch({ type: 'GET_MACARON_FLAVORS', payload: data }))
+));
+
+export const getBrownieBases = () => (dispatch => (
+  axios.get('/api/brownieBases')
+    .then(data => dispatch({ type: 'GET_BROWNIE_BASES', payload: data }))
+));
--- front/src/Reducers/brownie_bases_reducer.js
+++ front/src/Reducers/brownie_bases_reducer.js
@@ -1,31 +1,38 @@
 import amandes from '../Assets/Images/topping_amandes.png';
 import cacahuetes from '../Assets/Images/topping_cacahuetes.png';
 
-export default () => {
-  return [
-    {
-      id: 1,
-      name: 'Amandes caramélisées',
-      type: 'Base brownie',
-      size: 'S',
-      price: 4,
-      dispo: true,
-      info: 'some info',
-      img: amandes,
-      allerg: '',
-      compatible: [],
-    },
-    {
-      id: 2,
-      name: 'Cacahuètes',
-      type: 'Base brownie',
-      size: 'S',
-      price: 4,
-      dispo: true,
-      info: 'some info',
-      img: cacahuetes,
-      allerg: '',
-      compatible: [],
-    },
-  ];
+const initialState = [
+  {
+    id: 1,
+    name: 'Amandes caramélisées',
+    type: 'Base brownie',
+    size: 'S',
+    price: 4,
+    dispo: true,
+    info: 'some info',
+    img: amandes,
+    allerg: '',
+    compatible: [],
+  },
+  {
+    id: 2,
+    name: 'Cacahuètes',
+    type: 'Base brownie',
+    size: 'S',
+    price: 4,
+    dispo: true,
+    info: 'some info',
+    img: cacahuetes,
+    allerg: '',
+    compatible: [],
+  },
+];
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'GET_BROWNIE_BASES':
+      return action.payload;
+    default:
+      return state;
+  }
 };
