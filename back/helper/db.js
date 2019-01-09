c6ca962145264aec493ab78d7fc56d634d53b68b
Stephane Guinot <33290562+StephaneGNT@users.noreply.github.com>
2019-01-09 14:28:18 +0100

    Delete db.js

--- back/helper/db.js
+++
@@ -1,8 +0,0 @@
-const mysql      = require('mysql');
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'New4Pass415Ordi_',
-  database : 'pimp_my_cake_admin'
-});
-module.exports  =  connection;
