8686ebef85d6e73ce207b1188928a08dfa1ab4c0
Stephane Guinot <33290562+StephaneGNT@users.noreply.github.com>
2019-01-26 10:24:09 +0100

    Delete mailGunHelper.js

--- back/helper/mailGunHelper.js
+++
@@ -1,6 +0,0 @@
-const mailGunCredentials = {
-  api_key: 'pubkey-8f194a9373a1707137337fb0602336a1',
-  DOMAIN: "sandbox627d9c845056459f95b1034eeaff791d.mailgun.org",
-}
-
-module.exports = mailGunCredentials;
